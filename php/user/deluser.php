ab5b4934c6225cbdd07d4f9ac6fb0709807d3ac1
Mateusz <sobczak.mateusz27@gmail.com>
2024-06-11 15:54:14 +0200

    fpt

--- php/user/deluser.php
+++ php/user/deluser.php
@@ -5,7 +5,12 @@ if(isset($_POST["user"])&&$_POST["user"]!=0){
     $user = htmlspecialchars($_POST["user"]);
 
     $sql = "DELETE FROM osoby WHERE id_osoby = $user";
-    $reult = $conn -> query($sql);
+    $sql2 = "DELETE FROM typy WHERE id_osoby = $user";
+    $sql3 = "DELETE FROM zaglosowane WHERE id_osoby = $user";
+   
+    $result = $conn -> query($sql);
+    $result2 = $conn -> query($sql2);
+    $result2 = $conn -> query($sql3);
 
     header("location:../../adminpanel.php?deluser=1");
 }
